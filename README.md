b1ad1c13b66af20478b983d1c4fdf59ccd76d7f8
Jamie Morrissey <jamiemorrissey02@gmail.com>
2022-06-03 00:14:04 -0700

    added for loop

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # game_loop_test
+
+
+Game engine written in C<p>
+for hangman_c
---
+++ game_loop_test.c
@@ -0,0 +1,15 @@
+// game_loop_test.c
+
+#include <stdio.h>
+
+int main()
+{
+	// count town 1 through 3
+
+	for (int tries = 1; tries <= 3; tries++)
+	{
+		printf("%d\n", tries);
+	}
+
+	return 0;
+}
