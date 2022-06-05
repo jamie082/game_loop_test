852511c76c2ef4e17c319f885af7a4540ac1ec09
Jamie Morrissey <jamiemorrissey02@gmail.com>
2022-06-04 18:00:14 -0700

    copy to Stackoverflow

--- game_loop_test.c
+++ game_loop_test.c
@@ -5,11 +5,18 @@
 int main()
 {
 	// count town 1 through 3
-
-	for (int tries = 1; tries <= 3; ++tries)
+	
+	for (int tries = 1; tries < 3; tries++)
 	{
-		printf("Start Game: ");
-		printf("Game Loop program\t%d\n", tries);
+		printf("Start Game: \n");
+		printf("Game Loop program");
+		printf("\n");
+
+		if (tries == 3)
+		{
+			break;
+		}
+		tries++;
 	}
 
 	return 0;
